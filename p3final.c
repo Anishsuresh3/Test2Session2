4fa9b0fd5915036ee5e89b795a1775173eb5285f
Anish <96220428+Anishsuresh3@users.noreply.github.com>
2022-03-01 13:53:00 +0000

    ..

--- p3final.c
+++ p3final.c
@@ -1,1 +1,32 @@
-
+#include<stdio.h>
+int input_number()
+{
+  int n;
+  printf("Enter the number:\n");
+  scanf("%d",&n);
+  return n;
+}
+int is_prime(int n)
+{
+  int i,c=0;
+  for(i=1;i<=n;i++)
+  {
+    if(n%i==0)
+      c++;
+  }
+  return c;
+}
+void output(int n, int is_prime)
+{
+  if(is_prime>2)
+    printf("The number %d is not a prime number\n",n);
+  else
+    printf("The number %d is a prime number\n",n);
+}
+int main()
+{
+  int n=input_number();
+  int p=is_prime(n);
+  output(n,p);
+  return 0;
+}
--- p4final.c
+++ p4final.c
@@ -1,1 +1,32 @@
-
+#include<stdio.h>
+int input()
+{
+  int n;
+  printf("Enter the number:\n");
+  scanf("%d",&n);
+  return n;
+}
+int find_fibo(int n)
+{
+  int f=0,f1=1,s,i;
+  if(n<=1)
+    return n;
+  for(i=2;i<=n;i++)
+  {
+    s=f+f1;
+    f=f1;
+    f1=s;
+  }
+  return s;
+}
+void output(int n, int fibo)
+{
+  printf("The %dth number in fibonacci sequence is %d",n,fibo);
+}
+int main()
+{
+  int n=input();
+  int f=find_fibo(n);
+  output(n,f);
+  return 0;
+}
